4a58163b0d13b8d72a3f80160ef0da12dcde75e9
aben20807 <aben20807@gmail.com>
2017-07-20 18:05:06 +0800

    hello OuO

---
+++ hello/src/main.rs
@@ -0,0 +1,13 @@
+// Author: Huang Po-Hsuan <aben20807@gmail.com>
+// Filename: main.rs
+// Last Modified: 2017-07-20 17:24:05
+// Vim: enc=utf-8
+// Rust Version: 1.18.0
+// !rustc
+
+fn main() {
+    let mut name = "OuO";
+    println!("Hello, {}", name);
+    name = "好喔";
+    println!("Hello, {}", name);
+}
