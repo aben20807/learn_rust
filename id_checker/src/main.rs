2f76e4340995f200947f33b97bf32d29b3725e94
aben20807 <aben20807@gmail.com>
2017-08-12 22:38:45 +0800

    Can compare w/o newline

--- id_checker/src/main.rs
+++ id_checker/src/main.rs
@@ -1,11 +1,32 @@
-use std::io;
+use std::io::{self, BufRead};
 
 fn input() {
     println!("input id: ");
     let mut id = String::new();
-    io::stdin().read_line(&mut id)
-        .expect("Error");
-    println!("id: {}", id);
+    id.clear();
+    let stdin = io::stdin();
+    stdin.lock().read_line(&mut id)
+        .expect("input Error");
+    let id = id.trim();
+    // if let Some('\n')=id.chars().next_back() {
+    //     id.pop();
+    // }
+    // if let Some('\r')=id.chars().next_back() {
+    //     id.pop();
+    // }
+    check(&id)
+}
+
+fn check(id: &str) {
+    // if id.eq("DD") {
+    if id == "DD" {
+        println!("Y");
+    }
+    else {
+        println!("{}, {}", id.len(), "DD".to_owned().len());
+        println!("{}, {}", id, "DD");
+        println!("N");
+    }
 }
 
 fn main() {
