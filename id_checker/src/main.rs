1d839eca07e63e985f86977f37835e97fde90c66
aben20807 <aben20807@gmail.com>
2017-08-13 22:51:12 +0800

    Can handle first char to map code

--- id_checker/src/main.rs
+++ id_checker/src/main.rs
@@ -19,14 +19,16 @@ fn check(id: &str) {
         let id = id.to_uppercase();
         let id0 = &id[..1];
         // String IdStr="";
-        static IDCOUNTRY: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        let id_num: [i32; 26]=[10,11,12,13,14,15,16,17,34,18,19
+        static COUNTRY_LIST: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let country_list_code: [i32; 26]=[10,11,12,13,14,15,16,17,34,18,19
             ,20,21,22,35,23,24,25,26,27,28,29,32,30,31,33];
-
+        let country_index = COUNTRY_LIST.find(id0).unwrap();
+        let country_code = country_list_code[country_index];
+        let mut sum = 0;
+        sum += (country_code / 10) + (country_code % 10 * 9);
+        println!("{}", sum);
         for i in 1..(id.len()-1) {
-            println!("{}", i);
         }
-        println!("{}", id0);
     }
     else {
         println!("input id wrong format!\n");
