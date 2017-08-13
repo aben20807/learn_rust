de4d5f5007d8771e1fbf9539b2720861fdf081e8
aben20807 <aben20807@gmail.com>
2017-08-13 18:00:50 +0800

    Used subString, defined static string, array

--- id_checker/src/main.rs
+++ id_checker/src/main.rs
@@ -16,14 +16,24 @@ fn input() {
 fn check(id: &str) {
     let reg = Regex::new(r"^[a-zA-Z][12]\d{8}$").unwrap();
     if reg.is_match(id) {
-        println!("Y");
+        let id = id.to_uppercase();
+        let id0 = &id[..1];
+        // String IdStr="";
+        static IDCOUNTRY: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let id_num: [i32; 26]=[10,11,12,13,14,15,16,17,34,18,19
+            ,20,21,22,35,23,24,25,26,27,28,29,32,30,31,33];
+
+        for i in 1..(id.len()-1) {
+            println!("{}", i);
+        }
+        println!("{}", id0);
     }
     else {
-        println!("input id wrong format");
+        println!("input id wrong format!\n");
+        input();
     }
 }
 
 fn main() {
-    println!("Hello, world!");
     input();
 }
