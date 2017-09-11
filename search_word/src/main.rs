1b858822324faa163d9d4616162ee97085769a33
aben20807 <aben20807@gmail.com>
2017-09-11 23:21:25 +0800

    Bug when multiple space last word

--- search_word/src/main.rs
+++ search_word/src/main.rs
@@ -2,19 +2,19 @@
 
 fn search(s: &String, n: u32) -> &str{
     let bytes = s.as_bytes();
-    let (mut start, mut n): (usize, u32) = (0, n);
+    let (mut start, mut word_count): (usize, u32) = (0, 1);
+    let mut litem = b'0';
     for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' && n == 1 {
-            // println!("n == 1");
+        if word_count == n && item == b' ' {
             return &s[start..i];
         }
-        else if item == b' ' {
-            start = i+1;
-            n -= 1;
-            // println!("{}", n);
+        if item != b' ' && litem == b' ' {
+            start = i;
+            word_count += 1;
         }
+        litem = item;
     }
-    &s[start..]
+    &s[..] // not exist
 }
 
 fn main() {
@@ -25,11 +25,9 @@ fn main() {
         if s == "0" {
             break;
         }
-        // println!("{}", s);
         let a: u32;
         println!("input number:");
         scan!("{}", a);
-        // println!("{}", a);
         let word = search(&s, a);
         println!("The {} word is '{}'", a, word);
     }
