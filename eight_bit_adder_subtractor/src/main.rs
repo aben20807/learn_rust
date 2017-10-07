3d8b143faa6d2f396f47d4fbd82bc85febf785e4
aben20807 <aben20807@gmail.com>
2017-10-07 20:50:05 +0800

    Can convert to binary

--- eight_bit_adder_subtractor/src/main.rs
+++ eight_bit_adder_subtractor/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let s: String;
     scan!("{}\n", s);
     let (minuend, symbol, subtrahend) = statement_analysis(s);
-    println!("{}, {}, {}", minuend, subtrahend, symbol);
+    println!("{}, {}, {}", to_binary(minuend), to_binary(subtrahend), symbol);
 }
 
 fn statement_analysis(statement: String) -> (i32, char, i32) {
@@ -13,9 +13,12 @@ fn statement_analysis(statement: String) -> (i32, char, i32) {
         if i != 0 && (c == '+' || c == '-') {
             let a = String::from(&statement[..i]).parse::<i32>().unwrap();
             let b = String::from(&statement[i+1..]).parse::<i32>().unwrap();
-            // println!("symbol {}, {}", a, b);
             return (a, c, b)
         }
     }
     (0, '=', 0)
+}
+
+fn to_binary(n: i32) -> String {
+    format!("{:08b}", n)
 }
