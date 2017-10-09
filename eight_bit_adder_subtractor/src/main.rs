669280e976ef571c4550640ab34beb92a8798c1a
aben20807 <aben20807@gmail.com>
2017-10-09 10:12:10 +0800

    Can print add result

--- eight_bit_adder_subtractor/src/main.rs
+++ eight_bit_adder_subtractor/src/main.rs
@@ -4,8 +4,9 @@ fn main() {
     println!("Input: ");
     let s: String;
     scan!("{}\n", s);
-    let (minuend, symbol, subtrahend) = statement_analysis(s);
-    println!("{}, {}, {}", to_binary(minuend), to_binary(subtrahend), symbol);
+    let (a, symbol, b) = statement_analysis(s);
+    // println!("{} {} {}", to_binary(a), symbol, to_binary(b));
+    plus(to_binary(a), to_binary(b));
 }
 
 fn statement_analysis(statement: String) -> (i8, char, i8) {
@@ -21,4 +22,34 @@ fn statement_analysis(statement: String) -> (i8, char, i8) {
 
 fn to_binary(n: i8) -> String {
     format!("{:08b}", n)
+}
+
+fn plus(a: String, b: String) {
+    println!("\t{}\n+)\t{}", a, b);
+    let mut ans = String::from("");
+    let mut carry = 0i8;
+    let mut bit_s = 0i8;
+    for i in (0..8).rev() {
+        let bit_a = String::from(&a[i..i+1]).parse::<i8>().unwrap();
+        let bit_b = String::from(&b[i..i+1]).parse::<i8>().unwrap();
+        let bit_add = bit_a + bit_b + carry;
+        if  bit_add > 1 {
+            bit_s = if bit_add == 2 {0} else {1};
+            // println!("{}", bit_a);
+            carry = 1;
+        }
+        else {
+            bit_s = bit_add;
+            carry = 0;
+        }
+        // println!("{}", bit_s);
+        ans.push_str(&bit_s.to_string()[..]);
+    }
+    ans = ans.chars().rev().collect::<String>();
+    println!("─────────────────");
+    println!("\t{}\ncarry: {}", ans, carry);
+}
+
+fn minus(a: String, b: String) {
+
 }
