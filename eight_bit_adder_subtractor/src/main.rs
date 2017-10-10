980ba52f8cc79328400c95025d0b3335b62e8e84
aben20807 <aben20807@gmail.com>
2017-10-10 11:03:41 +0800

    Add maybe_overflow, and fn to check is overflow, minus has bugs

--- eight_bit_adder_subtractor/src/main.rs
+++ eight_bit_adder_subtractor/src/main.rs
@@ -2,18 +2,35 @@
 
 fn main() {
     println!("Input: ");
-    let s: String;
-    scan!("{}\n", s);
-    let (a, symbol, b) = statement_analysis(s);
-    if symbol == '+' {
-        plus(&dec_to_bin(a), &dec_to_bin(b), true);
+    // let s: String;
+    // scan!("{}\n", s);
+    // let (a, symbol, b) = statement_analysis(s);
+    // if symbol == '+' {
+    //     plus(&dec_to_bin(a), &dec_to_bin(b), true);
+    // }
+    // else if symbol == '-' {
+    //     minus(&dec_to_bin(a), &dec_to_bin(b), true);
+    // }
+    // else {
+    //     println!("Wrong symbol!");
+    // }
+    for i in -128..128 {
+        for j in -128..128 {
+            if is_overflow(plus(&dec_to_bin(i as i8), &dec_to_bin(j as i8), false)) {
+                print!("({}+{})", i, j);
+            }
+        }
+        println!("");
     }
-    else if symbol == '-' {
-        minus(&dec_to_bin(a), &dec_to_bin(b), true);
-    }
-    else {
-        println!("Wrong symbol!");
-    }
+    // for i in -128..128 {
+    //     for j in -128..128 {
+    //         if is_overflow(minus(&dec_to_bin(i as i8), &dec_to_bin(j as i8), false)) {
+    //             print!("({}-{})", i, j);
+    //         }
+    //     }
+    //     println!("");
+    // }
+    println!("\ndone");
 }
 
 fn statement_analysis(statement: String) -> (i8, char, i8) {
@@ -62,7 +79,7 @@ fn bin_to_dec(input: &String) -> i8 {
             ans = ans + base.pow(i as u32)*(String::from(&input[7-i..8-i]).parse::<i8>().unwrap());
         }
     }
-    else {
+    else { // FIXME 10000000 = -128
         let tmp = two_s_complement(&input, false);
         for i in 0..7 {
             ans = ans + base.pow(i as u32)*(String::from(&tmp[7-i..8-i]).parse::<i8>().unwrap());
@@ -92,7 +109,7 @@ fn two_s_complement(input: &String, can_display_process: bool) -> String {
     ans.0
 }
 
-fn plus(a: &String, b: &String, can_display_process: bool) -> (String, i8) {
+fn plus(a: &String, b: &String, can_display_process: bool) -> (String, i8, bool) {
     if can_display_process {
         println!("\n\t{} ({})\n+)\t{} ({})", a, bin_to_dec(&a), b, bin_to_dec(&b));
     }
@@ -118,20 +135,38 @@ fn plus(a: &String, b: &String, can_display_process: bool) -> (String, i8) {
         println!("─────────────────");
         println!("\t{} ({})\t[carry: {}]\n", ans, bin_to_dec(&ans), carry);
     }
-    (ans, carry)
+    let maybe_overflow =
+        (String::from(&a[..1]).parse::<i8>().unwrap() == 0 &&
+         String::from(&b[..1]).parse::<i8>().unwrap() == 0) ||
+        (String::from(&a[..1]).parse::<i8>().unwrap() == 1 &&
+         String::from(&b[..1]).parse::<i8>().unwrap() == 1);
+    (ans, carry, maybe_overflow)
 }
 
-fn minus(a: &String, b: &String, can_display_process: bool) -> (String, i8) {
+fn minus(a: &String, b: &String, can_display_process: bool) -> (String, i8, bool) {
     if can_display_process {
         println!("\n\t{} ({})\n-)\t{} ({})", a, bin_to_dec(&a), b, bin_to_dec(&b));
         println!("─────────────────");
     }
     let b_complement = two_s_complement(&b, false);
-    let (ans, carry) = plus(&a, &b_complement, false);
+    let (ans, mut carry, maybe_overflow) = plus(&a, &b_complement, false);
+    carry = (carry + 1)%2;
     if can_display_process {
         println!("\t{} ({})\n+)\t{} ({})\t[2's]", a, bin_to_dec(&a), b_complement, bin_to_dec(&b_complement));
         println!("─────────────────");
         println!("\t{} ({})\t[carry: {}]\n", ans, bin_to_dec(&ans), carry);
     }
-    (ans, carry)
+    let maybe_overflow =
+        (String::from(&a[..1]).parse::<i8>().unwrap() == 0 &&
+         String::from(&b[..1]).parse::<i8>().unwrap() == 1) ||
+        (String::from(&a[..1]).parse::<i8>().unwrap() == 1 &&
+         String::from(&b[..1]).parse::<i8>().unwrap() == 0);
+    (ans, carry, maybe_overflow)
+}
+
+fn is_overflow(input: (String, i8, bool)) -> bool {
+    if input.2 && String::from(&input.0[..1]).parse::<i8>().unwrap() != input.1 {
+        return true;
+    }
+    false
 }
