be50e3e064ca21d06fbe5a2997130c0e29fe29d8
aben20807 <aben20807@gmail.com>
2017-10-12 22:02:02 +0800

    Simplify code

--- eight_bit_adder_subtractor/src/main.rs
+++ eight_bit_adder_subtractor/src/main.rs
@@ -19,8 +19,8 @@ fn main() {
 fn statement_analysis(statement: String) -> (i8, char, i8) {
     for (i, c) in statement.chars().enumerate() {
         if i != 0 && (c == '+' || c == '-') {
-            let a = String::from(&statement[..i]).parse::<i8>().unwrap();
-            let b = String::from(&statement[i+1..]).parse::<i8>().unwrap();
+            let a = statement[..i].parse::<i8>().unwrap();
+            let b = statement[i+1..].parse::<i8>().unwrap();
             return (a, c, b)
         }
     }
@@ -29,7 +29,7 @@ fn statement_analysis(statement: String) -> (i8, char, i8) {
 
 fn dec_to_bin(input: i8) -> String {
     let mut dec:i32 = input as i32; // Because -128 * (-1) will overflow
-    let mut ans = String::from("");
+    let mut ans = String::new();
     let mut count = 0;
     let mut is_neg = false;
     if dec < 0 {
@@ -59,16 +59,16 @@ fn bin_to_dec(input: &String) -> i8 {
     let base = 2i8;
     if &input[..1] == "0" {
         for i in 0..7 {
-            ans = ans + base.pow(i as u32)*(String::from(&input[7-i..8-i]).parse::<i8>().unwrap());
+            ans = ans + base.pow(i as u32)*(input[7-i..8-i].parse::<i8>().unwrap());
         }
     }
     else {
         let tmp = two_s_complement(&input, false);
         for i in 0..7 {
-            ans = ans + base.pow(i as u32)*(String::from(&tmp[7-i..8-i]).parse::<i8>().unwrap());
+            ans = ans + base.pow(i as u32)*(tmp[7-i..8-i].parse::<i8>().unwrap());
         }
         ans = ans * (-1);
-        if ans == 0 && String::from(&input[..1]).parse::<i8>().unwrap() == 1 {
+        if ans == 0 && input[..1].parse::<i8>().unwrap() == 1 {
             ans = -128;
         }
     }
@@ -76,9 +76,9 @@ fn bin_to_dec(input: &String) -> i8 {
 }
 
 fn one_s_complement(input: &String) -> String {
-    let mut ans = String::from("");
+    let mut ans = String::new();
     for i in 0..8 {
-        let old_bit = String::from(&input[i..i+1]).parse::<i8>().unwrap();
+        let old_bit = input[i..i+1].parse::<i8>().unwrap();
         let bit = (old_bit + 1) % 2;
         ans.push_str(&bit.to_string()[..]);
     }
@@ -99,12 +99,12 @@ fn plus(a: &String, b: &String, can_display_process: bool) -> (String, i8, bool)
     if can_display_process {
         println!("\n\t{} ({})\n+)\t{} ({})", a, bin_to_dec(&a), b, bin_to_dec(&b));
     }
-    let mut ans = String::from("");
+    let mut ans = String::new();
     let mut carry = 0i8;
     let mut bit_s: i8;
     for i in (0..8).rev() {
-        let bit_a = String::from(&a[i..i+1]).parse::<i8>().unwrap();
-        let bit_b = String::from(&b[i..i+1]).parse::<i8>().unwrap();
+        let bit_a = a[i..i+1].parse::<i8>().unwrap();
+        let bit_b = b[i..i+1].parse::<i8>().unwrap();
         let bit_add = bit_a + bit_b + carry;
         if  bit_add > 1 {
             bit_s = if bit_add == 2 {0} else {1};
@@ -122,10 +122,10 @@ fn plus(a: &String, b: &String, can_display_process: bool) -> (String, i8, bool)
         println!("\t{} ({})\t[carry: {}]", ans, bin_to_dec(&ans), carry);
     }
     let maybe_overflow =
-        (String::from(&a[..1]).parse::<i8>().unwrap() == 0 &&
-         String::from(&b[..1]).parse::<i8>().unwrap() == 0) ||
-        (String::from(&a[..1]).parse::<i8>().unwrap() == 1 &&
-         String::from(&b[..1]).parse::<i8>().unwrap() == 1);
+        (a[..1].parse::<i8>().unwrap() == 0 &&
+         b[..1].parse::<i8>().unwrap() == 0) ||
+        (a[..1].parse::<i8>().unwrap() == 1 &&
+         b[..1].parse::<i8>().unwrap() == 1);
     (ans, carry, maybe_overflow)
 }
 
@@ -136,11 +136,11 @@ fn minus(a: &String, b: &String, can_display_process: bool) -> (String, i8, bool
     }
     let b_complement = two_s_complement(&b, false);
     let ans = plus(&a, &b_complement, false);
-    let maybe_overflow = &b[..] != "00000000" && // -0 never overflow
-        ((String::from(&a[..1]).parse::<i8>().unwrap() == 0 &&
-         String::from(&b[..1]).parse::<i8>().unwrap() == 1) ||
-        (String::from(&a[..1]).parse::<i8>().unwrap() == 1 &&
-         String::from(&b[..1]).parse::<i8>().unwrap() == 0));
+    let maybe_overflow = &b[..] != "00000000" && ( // -0 never overflow
+        (a[..1].parse::<i8>().unwrap() == 0 &&
+         b[..1].parse::<i8>().unwrap() == 1) ||
+        (a[..1].parse::<i8>().unwrap() == 1 &&
+         b[..1].parse::<i8>().unwrap() == 0));
     if can_display_process {
         println!("\t{} ({})\n+)\t{} ({})\t[2's]", a, bin_to_dec(&a), b_complement, bin_to_dec(&b_complement));
         println!("─────────────────");
@@ -151,7 +151,7 @@ fn minus(a: &String, b: &String, can_display_process: bool) -> (String, i8, bool
 
 #[allow(unused)]
 fn is_overflow(input: &(String, i8, bool)) -> bool {
-    if input.2 && String::from(&input.0[..1]).parse::<i8>().unwrap() != input.1 {
+    if input.2 && input.0[..1].parse::<i8>().unwrap() != input.1 {
         println!("\t\t\t[overflow: {}]", true);
         return true;
     }
